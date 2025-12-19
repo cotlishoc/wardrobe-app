d4ee293988eed01e7b8000f6b49cfda79e7ec350
cotlishoc <nastyanos684@gmail.com>
2025-12-19 20:08:24 +0300

    Fix bcrypt version

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,7 +34,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 origins = ["http://localhost:5173", "http://localhost:3000"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
